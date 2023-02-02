5249c3664d0a4ffc4410b0f003a1d1011d50081b
Danny Hsu <danny539425@gmail.com>
2023-02-02 11:02:39 +0800

    modify app.css add footer.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,27 +5,29 @@ import "./style/app.css";
 const App = () => {
   return (
     <div>
-      <section class="left">
-        <img src={require("./image/Photos.png")} alt="" />
-      </section>
-      <section class="right">
-        <h1>
-          <span class="C">C</span>hallenger
-        </h1>
-        <h1>
-          <span class="S">S</span>urviving
-        </h1>
-        <h1>
-          <span class="I">I</span>slands:
-        </h1>
-        <h1>
-          <span class="E">E</span>級玩家
-        </h1>
+      <div class="card">
+        <section class="left">
+          <img src={require("./image/Photos.png")} alt="" />
+        </section>
+        <section class="right">
+          <h1>
+            <span class="C">C</span>hallenger
+          </h1>
+          <h1>
+            <span class="S">S</span>urviving
+          </h1>
+          <h1>
+            <span class="I">I</span>slands:
+          </h1>
+          <h1>
+            <span class="E">E</span>級玩家
+          </h1>
 
-        <p>2023 師大資工營 NTNU CSIE CAMP</p>
-        <button>立即報名</button>
-        <br />
-      </section>
+          <p>2023 師大資工營 NTNU CSIE CAMP</p>
+          <button>立即報名</button>
+          <br />
+        </section>
+      </div>
     </div>
   );
 };
--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./style/style.css";
+import "./style/footer.css";
 
 const Footer = () => {
   return (
     <div>
-      <h1>聯絡方式</h1>
-      <p>example@gmail.com</p>
-      <p>0900-000000</p>
-      <div>
-        <img src="" alt="" />
+      <div class="card_foot">
+        <h1>聯絡方式</h1>
+        <p>example@gmail.com</p>
+        <p>0900-000000</p>
+        <div>
+          <img src="" alt="" />
+        </div>
       </div>
     </div>
   );
