7d9acf7a828bdd91ae62fd465bedff0506dd3765
Danny Hsu <danny539425@gmail.com>
2023-02-13 22:44:13 +0800

    add new page (router)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Nav from "./Nav";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Footer from "./Footer";
+
 ReactDOM.render(
   <React.StrictMode>
-    <Nav />
-    <App />
-    <Footer />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.querySelector("#root")
 );
---
+++ my-app/src/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const test = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
+        facere corporis aperiam ad sequi fuga ratione excepturi architecto
+        delectus maiores!
+      </p>
+    </div>
+  );
+};
