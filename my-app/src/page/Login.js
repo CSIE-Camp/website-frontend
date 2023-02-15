2cb7cc49afca716f0e02f22530675f10a1722bab
七詩パンダ <danal99151@gmail.com>
2023-02-15 17:50:27 +0800

    loginframe

--- my-app/src/page/Login.js
+++ my-app/src/page/Login.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "../style/login.css";
 
 export const Login = () => {
   return (
-    <div>
-      <h1>登入資訊</h1>
+    <div class = "login">
+      <h1 class = "title">登入報名資訊</h1>
+      <input class="register" placeholder="請輸入電子信箱"></input>
+      <input class="register" placeholder="請輸入密碼"></input>
+      <button class="confirm"><a href = "#next"><span class="arrow"></span></a></button>
+      <p class="help"><a href = "#forget">忘記密碼?</a><a href = "#regist">還沒註冊資工營帳號?</a></p>
     </div>
   );
 };
