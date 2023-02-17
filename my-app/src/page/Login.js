52371b031f4d9d3dfa672b3d53f8a32c4eb2f348
Danny Hsu <danny539425@gmail.com>
2023-02-17 10:25:11 +0800

    fix Login.js Link Problem

--- my-app/src/page/Login.js
+++ my-app/src/page/Login.js
@@ -1,14 +1,21 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 export const Login = () => {
   return (
-    <div class = "login">
-      <h1 class = "title">登入報名資訊</h1>
+    <div class="login">
+      <h1 class="title">登入報名資訊</h1>
       <input class="register" placeholder="請輸入電子信箱"></input>
       <input class="register" placeholder="請輸入密碼"></input>
-      <button class="confirm"><a href = "#next"><span class="arrow"></span></a></button>
-      <p class="help"><Link  to = "/forget">忘記密碼?</Link><Link to = "/regist">還沒註冊資工營帳號?</Link></p>
+      <button class="confirm">
+        <a href="#next">
+          <span class="arrow"></span>
+        </a>
+      </button>
+      <p class="help">
+        <Link to="/forget">忘記密碼?</Link>
+        <Link to="/regist">還沒註冊資工營帳號?</Link>
+      </p>
     </div>
   );
 };
