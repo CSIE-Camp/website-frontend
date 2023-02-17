e2fd25c64051aebba0ec64e527b504fca3267872
七詩パンダ <danal99151@gmail.com>
2023-02-17 11:19:09 +0800

    test

--- my-app/src/page/Info.js
+++ my-app/src/page/Info.js
@@ -7,12 +7,17 @@ export const Info = () => {
       <hr class="bar" size="0px" width="1336px"></hr>
       <p class="subTitle">報名相關資訊</p>
       <table class="mainInfor">
-      <tr class="context">1. 活動日期：2021/07/5 (一) ~ 2021/07/9 (五)</tr>
-      <tr class="context">2. 報名時間：即日起至 2021/05/17 (一) 止</tr>
-      <tr class="context">3. 錄取名單將於 2021/05/24 (四) 公佈於粉絲專頁</tr>
-      <tr class="context">4. 報名費用 (包含食宿、上課講義、營手冊、營服和保險等) : $6500/人</tr>
-      <tr class="context">5. 屆時請錄取者務必注意網站公佈之繳費流程</tr>
-      <tr class="context2">官方網址: <a target="_blank" href="https://www.facebook.com/ntnucsiecamp/">https://www.facebook.com/ntnucsiecamp/</a></tr>
+        <tr class="context">1. 活動日期：2021/07/5 (一) ~ 2021/07/9 (五)</tr>
+        <tr class="context">2. 報名時間：即日起至 2021/05/17 (一) 止</tr>
+        <tr class="context">3. 錄取名單將於 2021/05/24 (四) 公佈於粉絲專頁</tr>
+        <tr class="context">4. 報名費用 (包含食宿、上課講義、營手冊、營服和保險等) : $6500/人</tr>
+        <tr class="context">5. 屆時請錄取者務必注意網站公佈之繳費流程</tr>
+        <tr class="context2">
+          官方網址:{" "}
+          <a target="_blank" href="https://www.facebook.com/ntnucsiecamp/">
+            https://www.facebook.com/ntnucsiecamp/
+          </a>
+        </tr>
       </table>
       <p class="subTitle">注意事項和詳閱報名簡章</p>
       <table class="important">
@@ -25,7 +30,7 @@ export const Info = () => {
         </tr>
         <tr class="contextImport">
           3. 其餘詳細注意事項請參閱報名簡章:{" "}
-          <a target="_blank" href="https://reurl.cc/ZQdbj3" rel="noreferrer">
+          <a target="_blank" href="https://reurl.cc/ZQdbj3">
             https://reurl.cc/ZQdbj3
           </a>
         </tr>
