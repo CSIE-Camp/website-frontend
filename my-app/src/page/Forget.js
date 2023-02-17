7217d0af9241cdc2a933f8d0f624fcc65edda472
七詩パンダ <danal99151@gmail.com>
2023-02-17 17:58:51 +0800

    新增按鈕連結,輸入欄文字往右移動

--- my-app/src/page/Forget.js
+++ my-app/src/page/Forget.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Forget = () => {
   return (
     <div class="forget">
       <h1 class="title">登入報名資訊</h1>
-      <div class="email">
-        <input class="register button" placeholder="請輸入電子信箱"></input>
+      <div class="pwd_container">
+        <input class="register button" placeholder="    請輸入電子信箱"></input>
         <button class="send">
           <a href="#next">
             <span class="arrow"></span>
@@ -14,13 +14,13 @@ export const Forget = () => {
         </button>
       </div>
       <p class="help">* 請至電子信箱查收驗證信</p>
-      <input class="register" placeholder="建立新密碼"></input>
-      <div class="password">
-        <input class="register button" placeholder="再次輸入密碼"></input>
+      <input class="register" placeholder="    建立新密碼"></input>
+      <div class="pwd_container">
+        <input class="register button" placeholder="    再次輸入密碼"></input>
         <button class="confirm">
-          <a href="#next">
+          <Link to="/info">
             <span class="arrow"></span>
-          </a>
+          </Link>
         </button>
       </div>
     </div>
--- my-app/src/page/Regist.js
+++ my-app/src/page/Regist.js
@@ -1,18 +1,18 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Regist = () => {
   return (
     <div class="regist">
       <h1 class="title">登入報名資訊</h1>
-      <input class="register" placeholder="請輸入電子信箱"></input>
-      <input class="register" placeholder="請輸入密碼"></input>
-      <div>
-        <input class="register button" placeholder="再次輸入密碼"></input>
+      <input class="register" placeholder="    請輸入電子信箱"></input>
+      <input class="register" placeholder="    請輸入密碼"></input>
+      <div class="pwd_container">
+        <input class="register button" placeholder="    再次輸入密碼"></input>
         <button class="confirm">
-          <a href="#next">
+          <Link to="/info">
             <span class="arrow"></span>
-          </a>
+          </Link>
         </button>
       </div>
     </div>
