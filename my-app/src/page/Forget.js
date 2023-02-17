7c206c5f41fecda241b65a2f39f9a237e7f8d261
七詩パンダ <danal99151@gmail.com>
2023-02-17 10:56:30 +0800

    修正輸入框文字大小

--- my-app/src/page/Forget.js
+++ my-app/src/page/Forget.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../style/forget.css";
+import { Link } from "react-router-dom";
 
 export const Forget = () => {
   return (
--- my-app/src/page/Regist.js
+++ my-app/src/page/Regist.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../style/regist.css";
+import { Link } from "react-router-dom";
 
 export const Regist = () => {
   return (
