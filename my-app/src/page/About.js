64ef17b1080da2649999398ad7f61dc23c4e5044
othsueh <ych930719@gmail.com>
2023-02-21 18:49:15 +0800

    save for sleeping

--- my-app/src/page/About.js
+++ my-app/src/page/About.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div class="infoBox">
+      <h1>營隊介紹</h1>
+      <div class="intro">
+        <h2>主題介紹</h2>
+      </div>
+      <div class="intro">
+        <h2>課程介紹</h2>
+      </div>
     </div>
   );
 };
