9345172a00fe003a85646308be22d7cb2c0a033e
Danny Hsu <danny539425@gmail.com>
2023-02-25 22:31:01 +0800

    adjust app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,21 +15,21 @@ import Review from "./page/Review";
 const App = () => {
   return (
     <div className="App">
-      <div id="bg"></div>
-      <div id="container">
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/info" element={<Info />} />
-          <Route path="/more" element={<More />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/blank" element={<Blank />} />
-          <Route path="/forget" element={<Forget />} />
-          <Route path="/regist" element={<Regist />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/review" element={<Review />} />
-        </Routes>
+      <div id="bg">
+        <div id="container">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/info" element={<Info />} />
+            <Route path="/more" element={<More />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forget" element={<Forget />} />
+            <Route path="/regist" element={<Regist />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/review" element={<Review />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </div>
