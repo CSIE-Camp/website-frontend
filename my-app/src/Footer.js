5cbc2ec1028b3f11c52ae21e857f313f53e263a2
othsueh <ych930719@gmail.com>
2023-03-04 17:34:48 +0800

    fix footer ok

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -3,21 +3,38 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <h1>聯絡方式</h1>
-      <p>example@gmail.com</p>
-      <p>0900-000000</p>
+      <img class="island" id="left" src={require("./image/island left.png")} alt="island"></img>
+      <img class="island" id="left2" src={require("./image/island left 2.png")} alt="island"></img>
+      <img class="island" id="middle" src={require("./image/island middle.png")} alt="isl"></img>
+      <img
+        class="island"
+        id="right2"
+        src={require("./image/island right 2.png")}
+        alt="island"
+      ></img>
+      <img class="island" id="right" src={require("./image/island right.png")} alt="island"></img>
+      <h1 class="center">聯絡方式</h1>
+      <p id="email">example@gmail.com</p>
+      <p id="tel">0900-000000</p>
       <div class="icon">
         <a href="/">
-          <img src={require("./image/fb.png")} title="Facebook" alt="fb" />
+          <img class="link" src={require("./image/fb.png")} title="Facebook" alt="fb" />
         </a>
         <a href="/">
-          <img src={require("./image/ig.png")} title="IG" alt="ig" />
+          <img class="link" src={require("./image/ig.png")} title="IG" alt="ig" />
         </a>
         <a href="/">
-          <img src={require("./image/messenger.png")} title="Messenger" alt="messenger" />
+          <img
+            class="link"
+            src={require("./image/messenger.png")}
+            title="Messenger"
+            alt="messenger"
+          />
         </a>
       </div>
-      <p class="Copyright">Copyright © 2023 All rights reserved</p>
+      <p class="Copyright" id="copyright">
+        Copyright © 2023 All rights reserved
+      </p>
     </footer>
   );
 };
