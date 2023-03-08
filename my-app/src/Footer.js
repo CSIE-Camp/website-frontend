56a8a9c512b4313a3acf2c5a2c0c45041b466d9e
othsueh <ych930719@gmail.com>
2023-03-08 20:22:31 +0800

    footer fix finished

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -3,16 +3,23 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <img class="island" id="left" src={require("./image/island left.png")} alt="island"></img>
-      <img class="island" id="left2" src={require("./image/island left 2.png")} alt="island"></img>
-      <img class="island" id="middle" src={require("./image/island middle.png")} alt="isl"></img>
-      <img
-        class="island"
-        id="right2"
-        src={require("./image/island right 2.png")}
-        alt="island"
-      ></img>
-      <img class="island" id="right" src={require("./image/island right.png")} alt="island"></img>
+      <div id="picture">
+        <img class="island" id="left" src={require("./image/island left.png")} alt="island"></img>
+        <img
+          class="island"
+          id="left2"
+          src={require("./image/island left 2.png")}
+          alt="island"
+        ></img>
+        <img class="island" id="middle" src={require("./image/island middle.png")} alt="isl"></img>
+        <img
+          class="island"
+          id="right2"
+          src={require("./image/island right 2.png")}
+          alt="island"
+        ></img>
+        <img class="island" id="right" src={require("./image/island right.png")} alt="island"></img>
+      </div>
       <h1 class="center">聯絡方式</h1>
       <p id="email">example@gmail.com</p>
       <p id="tel">0900-000000</p>
