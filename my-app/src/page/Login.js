ee365ce03bcdc3f79b9f0763f19341d59b8709f9
othsueh <ych930719@gmail.com>
2023-04-10 23:49:55 +0800

    :::success Complete signupEmail function

--- my-app/src/page/Login.js
+++ my-app/src/page/Login.js
@@ -1,13 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { login } from "../api";
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    await login(email, password);
+  };
   return (
-    <form className="login">
+    <form className="login" onSubmit={handleLogin}>
       <h1 className="title">登入帳號</h1>
       <div className="inputFlex">
-        <input className="loginMail" placeholder="請輸入電子信箱" type={"email"} />
+        <input
+          className="loginMail"
+          placeholder="請輸入電子信箱"
+          type={"email"}
+          value={email || ""}
+          onChange={(event) => setEmail(event.target.value)}
+        />
         <div className="pwd_container">
-          <input className="loginPassword" placeholder="請輸入密碼" type={"password"} />
+          <input
+            className="loginPassword"
+            placeholder="請輸入密碼"
+            type={"password"}
+            value={password || ""}
+            onChange={(event) => setPassword(event.target.value)}
+          />
           <button className="confirm" type="submit">
             <span className="arrow"></span>
           </button>
